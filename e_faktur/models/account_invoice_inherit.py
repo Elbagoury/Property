7e22095d6ae6c0fbc3d627c8900b0f139c20cb97
yoshin4g4 <34533440+yoshin4g4@users.noreply.github.com>
2018-01-08 16:12:53 +0700

    Add files via upload

---
+++ e_faktur/models/account_invoice_inherit.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    nomor_faktur_pajak = fields.Char(string='Nomor Faktur Pajak', size=16)
+    faktur_pajak = fields.Many2one('faktur.pajak', string="Faktur Pajak", domain=[('state', '=', '0')])
+    npwp = fields.Char(string="NPWP")
+	
+    @api.onchange('partner_id')
+    def _onchange_customer_id(self):
+        if self.partner_id:
+            self.npwp = self.partner_id.npwp or ''	
+	
+    @api.multi
+    def action_create_faktur(self):
+        faktur_obj = self.env['faktur.pajak']
+        date = fields.Date.context_today(self)
+
+        for inv in self:
+            if inv.type == 'out_invoice':
+                vals = {
+                    'date_used': date,
+                    'invoice_id': inv.id,
+                    'partner_id': inv.partner_id.id,
+                    'pajak_type': 'out',
+                    'dpp': inv.amount_untaxed or 0.0,
+                    'tax_amount': inv.amount_tax or 0.0,
+                    'currency_id': inv.currency_id.id,
+                    'state': '1',
+                    }
+                inv.faktur_pajak.write(vals)
+            if inv.type == 'in_invoice':
+                if inv.nomor_faktur_pajak:
+                    kode_pt = inv.nomor_faktur_pajak[:3]
+                    kode_cabang = inv.nomor_faktur_pajak[3:6]
+                    tahun = inv.nomor_faktur_pajak[6:8]
+                    nomor_fp = inv.nomor_faktur_pajak[-8:]
+                    vals = {
+                        'date_used': date,
+                        'invoice_id': inv.id,
+                        'partner_id': inv.partner_id.id,
+                        'pajak_type': 'in',
+                        'dpp': inv.amount_untaxed or 0.0,
+                        'tax_amount': inv.amount_tax or 0.0,
+                        'currency_id': inv.currency_id.id,
+                        'state': '1',
+                        'nomor_perusahaan': kode_pt,
+                        'kode_cabang': kode_cabang,
+                        'tahun_penerbit': tahun,
+                        'nomor_urut': nomor_fp,
+                        }
+                    faktur_obj.create(vals)
+
+    @api.multi
+    def action_invoice_open(self):
+        for inv in self:
+            inv.action_create_faktur()
+        return super(AccountInvoice, self).action_invoice_open()
